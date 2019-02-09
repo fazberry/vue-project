77c574003c3456170575a6ccca7af5de34bf9df2
fazberry <rxfarry@googlemail.com>
2019-02-09 15:41:02 +0000

    components

--- js/main.js
+++ js/main.js
@@ -58,3 +58,14 @@ var app6 = new Vue({
   	}
 })
 
+var app7 = new Vue({
+  	el: '#app-7',
+  	data: {
+    	groceryList: [
+      		{ id: 0, text: 'Vegetables' },
+      		{ id: 1, text: 'Cheese' },
+      		{ id: 2, text: 'Whatever else humans are supposed to eat' }
+    	]
+  	}
+})
+
